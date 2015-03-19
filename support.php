14f037cd81f7919bf879375ec1f5970734e50c4a
Ninjoh <m.heil375@gmail.com>
2015-03-19 12:24:36 +0100

    Sync action

--- support.php
+++ support.php
@@ -113,8 +113,7 @@
 
 											
 											
-										<a class="twitter-timeline"  href="https://twitter.com/Technocraft375" data-widget-id="567377639871377408">Tweets by @Technocraft375</a>
-										<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+										 <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/Ninjoh375/lists/technocraft" data-widget-id="578513541902286848">Tweets from https://twitter.com/Ninjoh375/lists/technocraft</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> 
 						</div>
 					</div>
 					<!--Twitter panel end-->
